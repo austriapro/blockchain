e8fdedf53d5cbae504be83f55885d226227f6e90
chris2286266 <chris@web-lab.at>
2019-08-30 13:49:23 +0200

    First release

---
+++ multichain-samples/_readme.txt
@@ -0,0 +1,7 @@
+30.8.2019
+
+Demos for AustriaPro Blockchain Lab
+
+Write and read data to/from a blockchain using Multichain streams.
+
+See *.php files for code and *.log fils for sample output.
---
+++ multichain-samples/config.php
@@ -0,0 +1,17 @@
+<?php
+
+class cConfig{};
+
+//AustriaProLab@syn
+$cfg = new cConfig();
+$cfg->rpcHost = '192.168.10.42';
+$cfg->rpcPort = 17176;
+$cfg->rpcUser = 'multichainrpc';
+$cfg->rpcPass = '---ENTER---PASSPHRASE---HERE---';
+
+$cfg->stream = 'stream3';
+
+$cfg->client = 'AustriaPro Lab Client 1';
+$cfg->key = 'AP-key-1';
+
+?>
