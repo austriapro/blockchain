34de861fbae6f10fb42a5cb83d44d600505b011d
chris2286266 <chris@web-lab.at>
2022-05-02 13:12:00 +0200

    Update for apro-lab-2
    Setup blockchain apro-lab-2 incl. updated docker image for MultiChain 2.2.1

--- multichain-node-austriapro/runchain.sh
+++ multichain-node-austriapro/runchain.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -x
 
 echo "Setup multichain.conf"
-
-mkdir -p /root/.multichain/apro-lab-1/
-
-cat << EOF > /root/.multichain/apro-lab-1/multichain.conf
+mkdir -p /root/.multichain/apro-lab-2/
+cat << EOF > /root/.multichain/apro-lab-2/multichain.conf
 rpcuser=multichainrpc
-rpcpassword=5F1qcAXD6DZS1TfJ2PmZDAhEdFHFS8Vh857sXPF1MBm7
+rpcpassword=5F1rcAXD6DZS1TgJ2PmZDAhEdFHFS8Vh857sXPF1MBm3
 rpcallowip=0.0.0.0/0.0.0.0
-rpcport=7176
+rpcport=7770
 autosubscribe=assets,streams
 #Note: do not edit directly, file will be overwritten by runchain.sh next time docker starts
 EOF
 
-echo "Start the chain: apro-lab-1"
-multichaind apro-lab-1@88.99.145.156:7177
+echo "Start the chain: apro-lab-2"
+multichaind apro-lab-2@88.99.145.156:7771
