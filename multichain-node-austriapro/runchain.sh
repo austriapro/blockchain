43dd8db5717bd127e6cbd588c9dc86256f817168
chris2286266 <chris@web-lab.at>
2023-08-05 10:55:10 +0200

    Updated to Multichain 2.3.3

--- multichain-node-austriapro/runchain.sh
+++ multichain-node-austriapro/runchain.sh
@@ -4,6 +4,7 @@ echo "Setup multichain.conf"
 mkdir -p /root/.multichain/apro-lab-2/
 cat << EOF > /root/.multichain/apro-lab-2/multichain.conf
 rpcuser=multichainrpc
+#Change to other some password  
 rpcpassword=5F1rcAXD6DZS1TgJ2PmZDAhEdFHFS8Vh857sXPF1MBm3
 rpcallowip=0.0.0.0/0.0.0.0
 rpcport=7770
